1994ab3638b41fd73ed7f04dd1f97c3bf01483a9
John Ky <newhoggy@gmail.com>
2018-12-05 08:17:47 +1100

    Stuff

--- state-machine/aggressive-256-7/simd-state.c
+++ state-machine/aggressive-256-7/simd-state.c
@@ -16,11 +16,14 @@ void sm_process_chunk(
     uint32_t *out_phi_buffer) {  
   __m256i s = _mm256_set_epi64x(0, 0x03020100, 0, 0x03020100);
 
-  size_t part_length = in_length;
+  size_t part_length = in_length / 2;
 
-  for (size_t i = 0; i < part_length; i += 1) {
-    __m256i p = _mm256_set_epi64x(0, 0, 0, simd_phi_table_32       [in_buffer[i]]);
-    __m256i t = _mm256_set_epi64x(0, 0, 0, simd_transition_table_32[in_buffer[i]]);
+  uint8_t *buf0 = in_buffer;
+  uint8_t *buf1 = in_buffer + part_length;
+
+  for (size_t i = 0; i < part_length; i += 2) {
+    __m256i p = _mm256_set_epi64x(0, 0, 0, simd_phi_table_32       [buf0[i]]);
+    __m256i t = _mm256_set_epi64x(0, 0, 0, simd_transition_table_32[buf0[i]]);
     out_phi_buffer[i] = _mm256_extract_epi32(_mm256_shuffle_epi8(p, s), 4);
     s = _mm256_shuffle_epi8(t, s);
   }
