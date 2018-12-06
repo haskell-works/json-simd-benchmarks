c7027f9bbe8b4c912ecd0f3421b961424203714b
John Ky <newhoggy@gmail.com>
2018-12-06 19:08:53 +1100

    Move function to other file

--- state-machine/aggressive-256-8/simd-state.c
+++ state-machine/aggressive-256-8/simd-state.c
@@ -24,3 +24,30 @@ void sm_process_chunk(
 
   *inout_state = _mm_extract_epi64(s, 0);
 }
+
+void make_ib_bp_chunks(
+    uint8_t state,
+    uint32_t *in_phis,
+    size_t phi_length,
+    uint8_t *out_ibs,
+    uint8_t *out_ops,
+    uint8_t *out_cls) {
+  __m128i ib_offset = _mm_set_epi64x(0, 5 + state * 8);
+  __m128i op_offset = _mm_set_epi64x(0, 6 + state * 8);
+  __m128i cl_offset = _mm_set_epi64x(0, 7 + state * 8);
+
+  for (size_t i = 0; i < phi_length; i += 8) {
+    __m256i v_8 = *(__m256i *)&in_phis[i];
+    __m256i v_ib_8 = _mm256_sll_epi64(v_8, ib_offset);
+    __m256i v_op_8 = _mm256_sll_epi64(v_8, op_offset);
+    __m256i v_cl_8 = _mm256_sll_epi64(v_8, cl_offset);
+    uint8_t all_ibs = (uint8_t)_pext_u32(_mm256_movemask_epi8(v_ib_8), 0x11111111);
+    uint8_t all_ops = (uint8_t)_pext_u32(_mm256_movemask_epi8(v_op_8), 0x11111111);
+    uint8_t all_cls = (uint8_t)_pext_u32(_mm256_movemask_epi8(v_cl_8), 0x11111111);
+
+    size_t j = i / 8;
+    out_ibs[j] = all_ibs;
+    out_ops[j] = all_ops;
+    out_cls[j] = all_cls;
+  }
+}
